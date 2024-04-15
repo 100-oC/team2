e3c90cc2fc0589a30645b4ec3f19ad50a17b3c0c
100-oC <mitotonngoria@outlook.jp>
2024-04-15 11:15:05 +0900

    update title
    タイトル更新

--- src/Scene/SceneTitle.cpp
+++ src/Scene/SceneTitle.cpp
@@ -14,8 +14,19 @@ SceneTitle::~SceneTitle() {}
 // タイトル初期化
 void SceneTitle::InitTitle() 
 {
-	// タイトル画像の読込
-	handle = LoadGraph(titleImagePath);
+	for (int i = 0; i < TITLE_MAX_NUM; i++)
+	{
+		// タイトル画像の読込
+		handle[i] = LoadGraph(TITLE_IMAGE_PATH[i]);
+		y[i] = 0;
+	}
+
+	y[TITLE_TITLE] = 400;
+	y[TITLE_GROUND] = 100;
+
+	fade = 0;
+	lighting = false;
+	progress = 0;
 
 	//BGM読込
 
@@ -27,19 +38,104 @@ void SceneTitle::InitTitle()
 // タイトル通常処理
 void SceneTitle::StepTitle() 
 {
-	//エンターかスペースを押したら
-	if (Input::Key::Push(KEY_INPUT_RETURN) || Input::Key::Push(KEY_INPUT_SPACE)||
-		Input::Mouse::Push(MOUSE_INPUT_LEFT))
+	switch (progress)
 	{
-		g_CurrentSceneId = SCENE_ID_FIN_TITLE;
+	case 0:	//画面作り
+
+		//地面
+		if (y[TITLE_GROUND] <= 0)
+		{
+			y[TITLE_GROUND] = 0;
+		}
+		else
+		{
+			y[TITLE_GROUND] -= 2.0f;
+		}
+
+		//タイトル
+		if (y[TITLE_TITLE] <= 0)
+		{
+			y[TITLE_TITLE] = 0;
+		}
+		else
+		{
+			y[TITLE_TITLE] -= 5.0f;
+		}
+
+		//以上が完成していると進行
+		if (y[TITLE_GROUND] == 0 && y[TITLE_TITLE] == 0)
+		{
+			progress++;
+		}
+
+		break;
+
+	case 1:	//入力待ち
+
+		//UIの点滅
+		//現れる
+		if (lighting)
+		{
+			fade += 5;
+
+			if (fade >= 255)
+			{
+				lighting = false;
+			}
+		}
+		//消える
+		else
+		{
+			fade -= 5;
+
+			if (fade <= 0)
+			{
+				lighting = true;
+			}
+		}
+
+		//エンターかスペースを押したら
+		if (Input::Key::Push(KEY_INPUT_RETURN) || Input::Key::Push(KEY_INPUT_SPACE) ||
+			Input::Mouse::Push(MOUSE_INPUT_LEFT))
+		{
+			g_CurrentSceneId = SCENE_ID_FIN_TITLE;
+		}
+
+		break;
+
+
+	default:
+		break;
 	}
+
+	//画面未完成時に左クリックで完成
+	if (progress != 1 && Input::Mouse::Push(MOUSE_INPUT_LEFT))
+	{
+		//座標の設定
+		y[TITLE_GROUND] = 0;
+		y[TITLE_TITLE] = 0;
+
+		//入力待ちへ
+		progress = 1;
+	}
+	
 }
 
 // タイトル描画処理
 void SceneTitle::DrawTitle() 
 {
-	//画像描画
-	DrawGraph(0, 0, handle,true);
+	for (int i = 0; i < TITLE_MAX_NUM-1; i++)
+	{
+		//画像描画
+		DrawGraph(0, y[i], handle[i], true);
+	}
+	
+
+	//fadeで透明度変更
+	SetDrawBlendMode(DX_BLENDMODE_ALPHA, fade);
+	DrawGraphF(0, y[TITLE_GEMESTART], handle[TITLE_GEMESTART], true);	//タイトル
+	//表示を元に戻す
+	SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
 }
 
 //タイトル終了処理
--- src/Scene/SceneTitle.h
+++ src/Scene/SceneTitle.h
@@ -1,9 +1,34 @@
 #pragma once
 
+enum TITLE_IMAGE
+{
+	TITLE_BACKGROUND,
+	TITLE_TITLE,
+	TITLE_GROUND,
+	TITLE_GEMESTART,
+
+	TITLE_MAX_NUM,
+};
+
+const char TITLE_IMAGE_PATH[TITLE_MAX_NUM][255] =
+{
+	"data/title/backGround.png",
+	"data/title/title.png",
+	"data/title/ground.png",
+	"data/title/gameStart.png",
+};
+
 //タイトルクラス
 class SceneTitle {
 private:
-	int handle;
+	int handle[TITLE_MAX_NUM];
+
+	int y[TITLE_MAX_NUM];
+
+	int fade;
+
+	bool lighting;
+	int progress;
 
 public:
 	SceneTitle();
